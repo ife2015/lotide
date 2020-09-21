332732c55d8e7dd411cf294aa8622ae41edacb8b
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-21 20:27:15 +0000

    headTest changes

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,5 @@
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+const assertEqual = require('../assertEqual')
+//const assertEqual = require('../assertEqual.js');
 
 assertEqual(head([5,6,7]),5); 
