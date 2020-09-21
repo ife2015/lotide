c79002a72ddfbd7b66adca583c9dfffd73eeff20
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-21 20:50:45 +0000

    eqArrays Dry Code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😍😍😍😍😍 Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🤮🤮🤮🤮🤮 Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
-
 const eqArrays = function(array1, array2) {
 
   const firstArray = array1.map((element, index) =>{
@@ -15,8 +7,8 @@ const eqArrays = function(array1, array2) {
   return firstArray;
 }; 
 
+module.exports = eqArrays; 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); 
 
 
