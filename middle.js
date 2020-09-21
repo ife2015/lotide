4f21ff894fa10d41d178ffae37c23efed8ca23e6
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-21 21:08:46 +0000

    middle array finder dry test

--- middle.js
+++ middle.js
@@ -1,20 +1,5 @@
 'use strict'; 
 
-const eqArrays = function(array1, array2) {
-  const firstArray = array1.map((element, index) =>{
-    return element === array2[index]; 
-  }).every(element => element === true);
-  return firstArray; 
-}; 
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`😍😍😍😍😍 Assertion Passed: ${array1} === ${array2} `);
-  } else {
-    console.log(`🤮🤮🤮🤮🤮 Assertion Failed: ${array1} !== ${array2} `);
-  }
-};
-
 const middle = function(arr) {
   // even
   if (arr.length % 2 === 0) {
@@ -34,4 +19,5 @@ const middle = function(arr) {
   }
 }
 
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4])
+module.exports = middle; 
+
